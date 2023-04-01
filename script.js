307c0178134c6e477b0469ed83c85f799b968522
Emmatsoka <emmatsoka.qwq@gmail.com>
2023-04-01 17:33:39 +0200

    .

---
+++ script.js
@@ -0,0 +1,23 @@
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 500);
+  const kepek = ["./backg/1.jpg","./backg/2.png","./backg/3.jpg","./backg/4.jpg","./backg/5.jpg","./backg/6.jpg",
+"./backg/7.jpg","./backg/8.jpg","./backg/9.png",];
+
+// Véletlenszerűen válasszuk ki egy képet
+const kepIndex = Math.floor(Math.random() * kepek.length);
+const kep = kepek[kepIndex];
+
+// Állítsuk be a header háttereként a kiválasztott képet
+const header = document.querySelector("header");
+header.style.backgroundImage = `url(${kep})`;
+}
+
+function showPage() {
+  var loader = document.getElementById("loader");
+  var egesz =   document.getElementById("egesz");
+  loader.classList.add("hide");
+  egesz.classList.add("aktiv");
+document.body.style.overflow = "visible";
+}
